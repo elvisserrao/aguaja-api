e7b2b81604f293784ee2fe75750ee1c329f474aa
Elvis Serrão <elvis.serrao1@gmail.com>
2020-11-09 23:43:34 -0300

    Modifica a action welcome para persistir um objeto Teste no banco de dados

--- src/main/java/com/aguaja/api/controller/HomeController.java
+++ src/main/java/com/aguaja/api/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.aguaja.api.controller;
 
+import com.aguaja.api.domain.Teste;
+import com.aguaja.api.repositories.TesteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+    @Autowired
+    TesteRepository tr;
 
     @GetMapping("")
-    public String welcome() {
-        return "Bem vindo";
+    public Teste welcome() {
+        Teste usu;
+        usu = new Teste( 1L, "Elvis", 30);
+        tr.save(usu);
+
+        return usu;
     }
 }
