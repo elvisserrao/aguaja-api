ca096c04f569a807fc2b0d7013063adc6078d664
Elvis Serrão <elvis.serrao1@gmail.com>
2020-11-09 23:42:38 -0300

    Cria interface repository para teste de persistência

---
+++ src/main/java/com/aguaja/api/repositories/TesteRepository.java
@@ -0,0 +1,9 @@
+package com.aguaja.api.repositories;
+
+import com.aguaja.api.domain.Teste;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TesteRepository extends JpaRepository<Teste, Long> {
+
+}
+
