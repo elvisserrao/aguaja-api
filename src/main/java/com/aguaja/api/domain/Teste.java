14f6539851fbe7cc771e5e943b9f6a43425478fb
Elvis Serrão <elvis.serrao1@gmail.com>
2020-11-09 23:41:44 -0300

    Cria classe para teste de persistência

---
+++ src/main/java/com/aguaja/api/domain/Teste.java
@@ -0,0 +1,55 @@
+package com.aguaja.api.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_TESTE")
+public class Teste implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "idade")
+    private Integer idade;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+
+    public Teste() {
+
+    }
+    public Teste(Long id, String nome, Integer idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
