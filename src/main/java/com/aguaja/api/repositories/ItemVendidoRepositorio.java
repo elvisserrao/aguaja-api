e74a07dfe920071246d39e42464027fd0e6fcd6f
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-11-20 20:57:53 -0300

    Cria repositorio para itemVendido

---
+++ src/main/java/com/aguaja/api/repositories/ItemVendidoRepositorio.java
@@ -0,0 +1,10 @@
+package com.aguaja.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aguaja.api.domain.Cliente;
+import com.aguaja.api.domain.ItemVendido;
+
+public interface ItemVendidoRepositorio extends JpaRepository<ItemVendido, Long>{
+
+}
