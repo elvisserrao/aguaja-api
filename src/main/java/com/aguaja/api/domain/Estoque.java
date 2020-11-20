0e2b22726438fa52bbd19b7ccac5e17a4a957259
Rafael Pinto <rafael12wq@gmail.com>
2020-11-19 22:53:01 -0300

    Cria a entidade estoque

---
+++ src/main/java/com/aguaja/api/domain/Estoque.java
@@ -0,0 +1,100 @@
+package com.aguaja.api.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "tb_estoque")
+public class Estoque implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer quantidade;
+	private Instant data_entrada;
+	private Double preco_custo;
+	private Double preco_venda;
+	
+	
+	public Estoque() {
+		super();
+	}
+	public Estoque(Integer id, Integer quantidade, Instant data_entrada, Double preco_custo, Double preco_venda) {
+		super();
+		this.id = id;
+		this.quantidade = quantidade;
+		this.data_entrada = data_entrada;
+		this.preco_custo = preco_custo;
+		this.preco_venda = preco_venda;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Instant getData_entrada() {
+		return data_entrada;
+	}
+	public void setData_entrada(Instant data_entrada) {
+		this.data_entrada = data_entrada;
+	}
+	public Double getPreco_custo() {
+		return preco_custo;
+	}
+	public void setPreco_custo(Double preco_custo) {
+		this.preco_custo = preco_custo;
+	}
+	public Double getPreco_venda() {
+		return preco_venda;
+	}
+	public void setPreco_venda(Double preco_venda) {
+		this.preco_venda = preco_venda;
+	}
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estoque other = (Estoque) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
