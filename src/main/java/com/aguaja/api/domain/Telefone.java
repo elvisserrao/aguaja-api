6e1974664acf03b0729ab53849f4dbf4466d6dcc
Rafael Pinto <rafael12wq@gmail.com>
2020-11-19 22:55:53 -0300

    Cria entidade telefone

---
+++ src/main/java/com/aguaja/api/domain/Telefone.java
@@ -0,0 +1,91 @@
+package com.aguaja.api.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_telefone")
+public class Telefone implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String ddd;
+	private String numero;
+	
+	
+	@OneToOne(mappedBy = "telefone")
+	private Cliente cliente;
+	
+	
+	
+	
+	public Telefone() {
+		super();
+	}
+	public Telefone(Integer id, String ddd, String numero) {
+		super();
+		this.id = id;
+		this.ddd = ddd;
+		this.numero = numero;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDdd() {
+		return ddd;
+	}
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Telefone other = (Telefone) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	 
+}
