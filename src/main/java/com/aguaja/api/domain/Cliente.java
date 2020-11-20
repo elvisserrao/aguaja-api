0bcadb648abfbe869b08a79fdffe73714331aa8e
Rafael Pinto <rafael12wq@gmail.com>
2020-11-19 22:50:37 -0300

    Cria a entidade cliente

---
+++ src/main/java/com/aguaja/api/domain/Cliente.java
@@ -0,0 +1,115 @@
+package com.aguaja.api.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private Integer sexo;
+	private String email;
+	private Instant dataNascimento;
+	private String login;
+	private String senha;
+	
+	
+	
+	public Cliente() {
+	}
+	
+	public Cliente(Integer id, String nome, Integer sexo, String email, Instant dataNascimento, String login, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.sexo = sexo;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getSexo() {
+		return sexo;
+	}
+	public void setSexo(Integer sexo) {
+		this.sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Instant getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Instant dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
