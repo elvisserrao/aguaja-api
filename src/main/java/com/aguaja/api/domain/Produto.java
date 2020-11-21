7c5924e310089baca952dcb843ecc05172592ca3
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-11-21 14:23:06 -0300

    Remove imports nao utilizados

--- src/main/java/com/aguaja/api/domain/Produto.java
+++ src/main/java/com/aguaja/api/domain/Produto.java
@@ -2,9 +2,7 @@ package com.aguaja.api.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
