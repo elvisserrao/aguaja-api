d4268929b28fddacd5cf56529c9149441e9e0cab
Rafael Pinto <rafael12wq@gmail.com>
2020-11-22 12:40:08 -0300

    Cria classe PedidoServico

---
+++ src/main/java/com/aguaja/api/services/ProdutoServico.java
@@ -0,0 +1,66 @@
+package com.aguaja.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.aguaja.api.domain.Produto;
+import com.aguaja.api.repositories.ProdutoRepositorio;
+import com.aguaja.api.services.exceptions.DatabaseException;
+import com.aguaja.api.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProdutoServico {
+	@Autowired
+	private ProdutoRepositorio repository;
+	
+	public List<Produto> findAll(){
+		return repository.findAll();
+	}
+	
+	public Produto findById(Long id) {
+		Optional<Produto> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Produto insert(Produto obj) {
+		repository.save(obj);
+		return obj;
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+	}
+	
+	public Produto update(Long id, Produto obj) {
+		
+		try {			
+			Produto entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
+	}
+
+	private void updateData(Produto entity, Produto obj) {
+		entity.setNome(obj.getNome());
+		entity.setLitros(obj.getLitros());
+		entity.setDescricao(obj.getDescricao());
+	}
+}
