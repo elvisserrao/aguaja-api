967bd968c13033f17a81594dbfc5cf667e2f2159
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-12-03 21:37:39 -0300

    Adiciona findByUsername para client e seller repositories

--- src/main/java/com/aguaja/api/repositories/SellerRepository.java
+++ src/main/java/com/aguaja/api/repositories/SellerRepository.java
@@ -1,9 +1,11 @@
 package com.aguaja.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aguaja.api.domain.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, Long>{
-
+    Optional<Seller> findByUsername(String username);
 }
