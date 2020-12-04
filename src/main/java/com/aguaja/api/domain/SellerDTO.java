47320a905aa5855ca3460210a18ea6607adf7916
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-12-03 21:35:13 -0300

    Cria classes ClientDTO e SellerDTO

---
+++ src/main/java/com/aguaja/api/domain/SellerDTO.java
@@ -0,0 +1,64 @@
+package com.aguaja.api.domain;
+import java.time.Instant;
+
+public class SellerDTO {
+	private String name;
+	private Integer gender;
+	private String email;
+	private Phone phone;
+	private Instant birthDate;
+	private String username;
+	private String password;
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Phone getPhone() {
+		return phone;
+	}
+	public void setPhone(Phone phone) {
+		this.phone = phone;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
