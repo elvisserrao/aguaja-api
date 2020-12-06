fad504f114a435b8858cd5e3df34750e18a123e9
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-12-06 14:44:52 -0300

    Corrige classe Product

--- src/main/java/com/aguaja/api/controller/ProductController.java
+++ src/main/java/com/aguaja/api/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.aguaja.api.domain.Product;
 import com.aguaja.api.services.ProductServico;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/product")
 public class ProductController {
