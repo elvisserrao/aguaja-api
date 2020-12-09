a279b5fcce9e1fac56a158599bef09f990511c37
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-12-09 19:01:45 -0300

    Corrige metodo para criar Order

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -4,22 +4,17 @@ import { Modal, Button, Table } from "react-bootstrap";
 import OrderService from "../services/order.service";
 
 function StockModal(props) {
-  useEffect(() => {
-    console.log(props.stocks, props.product);
-  });
-
   const createOrder = (stock) => {
     const data = {
-      price: stock.price,
+      price: stock.costPrice,
       orderStatus: 1,
       date: Date.now(),
-      client_id: props.user.id,
-      seller_id: stock.seller_id,
-      item: {
-        stock_id: stock.id,
-        quantity: 1,
-      },
+      clientId: props.user.id,
+      sellerId: stock.seller.id,
+      stockId: stock.id,
+      quantity: 1,
     };
+    console.log(data);
     OrderService.create(data)
       .then((response) => {
         console.log(response.data);
@@ -41,6 +36,7 @@ function StockModal(props) {
           Escolha seu produto!
         </Modal.Title>
       </Modal.Header>
+
       <Modal.Body>
         <Table striped bordered hover variant="dark">
           <thead>
@@ -51,6 +47,7 @@ function StockModal(props) {
               <th></th>
             </tr>
           </thead>
+
           <tbody>
             {props.stocks &&
               props.stocks.map((stock) => (
@@ -58,7 +55,7 @@ function StockModal(props) {
                   <td>1</td>
                   <td>{props.product.name}</td>
                   <td>R${stock.costPrice}</td>
-                  <Button onClick={createOrder} variant="primary">
+                  <Button onClick={() => createOrder(stock)} variant="primary">
                     Realizar pedido!
                   </Button>
                 </tr>
@@ -66,6 +63,7 @@ function StockModal(props) {
           </tbody>
         </Table>
       </Modal.Body>
+
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
