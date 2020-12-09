d3ab84b0d4b7675b805cc1aa699b451fa47e0076
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-12-09 19:05:49 -0300

    Muda a rota Post do Order e cria uma rota Get

--- src/main/java/com/aguaja/api/controller/OrderController.java
+++ src/main/java/com/aguaja/api/controller/OrderController.java
@@ -17,43 +17,82 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aguaja.api.domain.Order;
+import com.aguaja.api.domain.OrderDTO;
 import com.aguaja.api.domain.OrderItem;
+import com.aguaja.api.domain.Seller;
+import com.aguaja.api.domain.Stock;
+import com.aguaja.api.domain.Client;
+import com.aguaja.api.services.ClientService;
 import com.aguaja.api.services.OrderServico;
+import com.aguaja.api.services.SellerServico;
+import com.aguaja.api.services.StockServico;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 	@Autowired
-	private OrderServico service;
+	private OrderServico orderService;
+	
+	@Autowired
+	private ClientService clientService;
+	
+	@Autowired
+	private SellerServico sellerService;
+
+	@Autowired
+	private StockServico stockService;
 	
 	@GetMapping
 	public ResponseEntity<List<Order>> findAll(){
-		List<Order> list = service.findAll();
+		List<Order> list = orderService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Order> findById(@PathVariable Long id) {
-		Order obj = service.findById(id);
+		Order obj = orderService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	
+	}
+
+	@GetMapping(value = "/open")
+	public ResponseEntity<List<Order>> findByStatusOpen() {
+		List<Order> obj = orderService.findByStatusOpen();
 		return ResponseEntity.ok().body(obj);
 	}
 	
 	@PostMapping
-	public ResponseEntity<Order> insert(@RequestBody Order obj, @RequestBody OrderItem orderItem){
-		obj = service.insert(obj, orderItem);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj);
+	public ResponseEntity<Order> insert(@RequestBody OrderDTO obj){
+		Seller seller = sellerService.findById(obj.getSellerId());
+		Client client = clientService.findById(obj.getClientId());
+		Stock stock = stockService.findById(obj.getStockId());
+
+		Order order = new Order();
+
+		order.setDate(obj.getDate());
+		order.setOrderStatus(obj.getOrderStatus());
+		order.setPrice(obj.getPrice());
+		order.setSeller(seller);
+		order.setClient(client);
+
+		OrderItem item = new OrderItem(order, stock, obj.getQuantity());
+		
+		order = orderService.insert(order, item);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(order.getId()).toUri();
+		return ResponseEntity.created(uri).body(order);
 	}
 	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete( @PathVariable Long id){
-		service.delete(id);
+		orderService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order obj){
-		obj = service.update(id, obj);
+		obj = orderService.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
