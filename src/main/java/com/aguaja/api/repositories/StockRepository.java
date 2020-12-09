2937ee040b4164ebdde9dc6befc27b28a776b5b1
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-12-08 21:19:51 -0300

    Cria metodo findByProduct no StockRepository

--- src/main/java/com/aguaja/api/repositories/StockRepository.java
+++ src/main/java/com/aguaja/api/repositories/StockRepository.java
@@ -3,8 +3,11 @@ package com.aguaja.api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import com.aguaja.api.domain.Product;
 import com.aguaja.api.domain.Stock;
 @Transactional
 public interface StockRepository extends JpaRepository<Stock, Long>{
-
+    List<Stock> findByProduct(Product product);
 }
